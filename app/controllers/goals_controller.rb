43b5a5d8f417c5548b2ef90491ff98b749eda58e
Karle Durante <kdurante@customink.com>
2012-11-03 11:48:37 -0600

    Adding ability to update goal via sms.

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,30 +1,41 @@
 require 'twilio-ruby'
 
 class GoalsController < ApplicationController
+  before_filter :find_goal
+
   def edit
-    @goal = Goal.first || Goal.new
   end
 
   def update
-    goal = Goal.first || Goal.new
+    @goal.shirts_sold = params[:shirts_sold]
+    @goal.dollars_raised = params[:dollars_raised]
+    @goal.total_shirt_goal = params[:total_shirt_goal]
+    @goal.campaign_end_date = Date.civil(params[:campain_date][:year].to_i, params[:campain_date][:month].to_i, params[:campain_date][:day].to_i)
 
-    goal.shirts_sold = params[:shirts_sold]
-    goal.dollars_raised = params[:dollars_raised]
-    goal.total_shirt_goal = params[:total_shirt_goal]
-
-    goal.campaign_end_date = Date.civil(params[:campain_date][:year].to_i, params[:campain_date][:month].to_i, params[:campain_date][:day].to_i)
-
-    goal.save
+    @goal.save!
 
     render 'goals/save'
   end
 
   def sms
 
+    shirts_sold = params[:body].to_i
+    dollars_raised = shirts_sold * 25
+
+    @goal.shirts_sold = shirts_sold
+    @goal.dollars_raised = dollars_raised
+    @goal.save!
+
     twiml = Twilio::TwiML::Response.new do |r|
-      r.Sms "params: #{params.to_s}"
+      r.Sms "updated goal: #{shirts_sold} shirts sold, $#{dollars_raised} raised."
     end
+
     render :xml => twiml.text
   end
 
+
+  private
+  def find_goal
+    @goal = Goal.first || Goal.new
+  end
 end
